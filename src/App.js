f6dd6f3f344b057c218436022a5da7840b04d11a
bernardrayoso <rayosobernard@gmail.com>
2022-06-30 03:23:01 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import CardList from './components/card-list/CardList.component';
+
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			monsters: [],
+			searchField: '',
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) =>
+				this.setState(() => {
+					return { monsters: users };
+				})
+			);
+	}
+
+	onSearchChange = (event) => {
+		const searchField = event.target.value.toLocaleLowerCase();
+		this.setState(() => {
+			return { searchField: searchField };
+		});
+	};
+
+	render() {
+		const { monsters, searchField } = this.state;
+
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
+		});
+
+		return (
+			<div className='App'>
+				<input
+					className='search-box'
+					type='search'
+					placeholder='Search'
+					onChange={this.onSearchChange}
+				/>
+
+				<CardList monsters={filteredMonsters} />
+			</div>
+		);
+	}
+}
+
+export default App;
